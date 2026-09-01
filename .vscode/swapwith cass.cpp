fc2113b5f70996c586955a78486d03979aee8bec
VaishnaviGupta05 <vaishnavi.25b01010636@abes.ac.in>
2026-09-01 09:57:44 +0530

    commit

---
+++ .vscode/swapwith cass.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class SwapNumbers
+{
+private:
+	int first;
+	int second;
+
+public:
+	void getData()
+	{
+		cout << "Enter two numbers: ";
+		cin >> first >> second;
+	}
+
+	void swapValues()
+	{
+		int temporary = first;
+		first = second;
+		second = temporary;
+	}
+
+	void display()
+	{
+		cout << "First number = " << first << ", Second number = " << second << endl;
+	}
+};
+
+int main()
+{
+	SwapNumbers numbers;
+
+	numbers.getData();
+	cout << "Before swapping: ";
+	numbers.display();
+
+	numbers.swapValues();
+	cout << "After swapping: ";
+	numbers.display();
+
+	return 0;
+}
